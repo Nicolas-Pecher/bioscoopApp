81cbf62329d706bc93d611885d8792f3282a8242
Nicolas Pecher <nicolas.pecher@student.ehb.be>
2019-04-10 15:45:56 +0200

    display movies on home page

--- bioscoopApp/movies/admin.py
+++ bioscoopApp/movies/admin.py
@@ -4,7 +4,36 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(Movie)
+class ProducerInline(admin.TabularInline):
+    model = Producer.movie.through
+    extra = 1
+
+class DirectorInline(admin.TabularInline):
+    model = Director.movie.through
+    extra = 1
+
+class ActorInline(admin.TabularInline):
+    model = Actor.movie.through
+    extra = 3
+
+class MovieAdmin(admin.ModelAdmin):
+    inlines = [
+        ProducerInline,DirectorInline,ActorInline
+    ]
+    exclude = ('movie',)
+'''
+class DirectorInline(admin.TabularInline):
+    model = Director
+    extra = 1
+
+class ProducerInline(admin.TabularInline):
+    model = Producer
+    extra = 1
+
+class MovieAdmin(admin.ModelAdmin):
+    inlines = [DirectorInline,ProducerInline]
+'''
+admin.site.register(Movie,MovieAdmin)
 admin.site.register(Actor)
 admin.site.register(Director)
 admin.site.register(Producer)
--- bioscoopApp/movies/static/movies/js/home.js
+++ bioscoopApp/movies/static/movies/js/home.js
@@ -5,8 +5,8 @@ $(document).ready(function () {
     $('nav .trending').addClass("navColor");
     console.log("js working")
 
-    var newDistance = $('#new').offset().top;
-    var bestRatedDistance = $('#bestRated').offset().top;
+    var newDistance = $('#new').offset().top-1;
+    var bestRatedDistance = $('#bestRated').offset().top-1;
     var upcommingDistance = $('#upcomming').offset().top - 1;
 
     $(window).scroll(function () {
@@ -30,22 +30,32 @@ function changeHighlight(highlight) {
             $('nav .trending').removeClass("navColor");
             $('nav .new').addClass('navColor');
             $('nav .bestRated').removeClass('navColor');
+            $('nav .upcomming').removeClass('navColor');
+            $('nav').css("background-color","#2A9D8F");
             break;
 
         case 'trending':
             $('nav .trending').addClass("navColor");
             $('nav .new').removeClass('navColor');
+            $('nav .bestRated').removeClass('navColor');
+            $('nav .upcomming').removeClass('navColor');
+            $('nav').css("background-color","#264653");
             break;
 
         case 'bestRated':
+            $('nav .trending').removeClass("navColor");
             $('nav .new').removeClass('navColor');
             $('nav .bestRated').addClass('navColor');
             $('nav .upcomming').removeClass('navColor');
+            $('nav').css("background-color","#E9C46A");
             break;
 
         case 'upcomming':
+            $('nav .trending').removeClass("navColor");
+            $('nav .new').removeClass('navColor');
             $('nav .bestRated').removeClass('navColor');
             $('nav .upcomming').addClass('navColor');
+            $('nav').css("background-color","#F4A261");
             break;
 
         default:
